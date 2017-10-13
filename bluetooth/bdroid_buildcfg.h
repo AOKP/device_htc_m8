4af86e8ae56eb6ac4bcdf5fe66d8d834424b88c6
Stefano Mura [STELIX PippoX3] <ssspinni@gmail.com>
2017-10-13 16:18:44 +0200

    Revert "Revert "bluetooth: Our kernel is missing CLOCK_BOOTTIME_ALARM (alarmtimer)""
    This reverts commit a60766f9c364438693d9bd533478ab25ea89a4d1.
    
    Change-Id: I43a9bd31b6b900eb132b1c2d93c00d5b0c32c277

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,4 +23,7 @@
 #define BLUETOOTH_QTI_SW          TRUE
 #define BT_CLEAN_TURN_ON_DISABLED TRUE
 
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
+
 #endif
